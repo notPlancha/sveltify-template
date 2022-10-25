2f089c5587c7235343f3e3fe86d9a08adc57b8be
notPlancha <andre.plancha@hotmail.com>
2022-10-25 21:47:05 +0100

    changed to polling for consistency

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import "svelte";
 import Button from "./lib/button.svelte";
 async function main() {
-    while (!Spicetify?.showNotification) {
+    while (!Spicetify?.showNotification || document.getElementsByClassName("player-controls__right").length === 0) {
         await new Promise(resolve => setTimeout(resolve, 100));
     }
+
     // https://svelte.dev/docs#run-time-client-side-component-api-creating-a-component
     const button = new Button({
         target: document.getElementsByClassName("player-controls__right")[0],
